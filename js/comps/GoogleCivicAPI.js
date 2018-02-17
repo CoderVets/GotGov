6a221999186492d94f48fe83bd1135c29bde9d9c
Sm00gly <32003644+Sm00gly@users.noreply.github.com>
2018-02-17 16:41:57 -0600

    working appv 2

--- js/comps/GoogleCivicAPI.js
+++ js/comps/GoogleCivicAPI.js
@@ -2,7 +2,7 @@
 import React, { Component } from 'react';
 import { ActivityIndicator, ListView, Text, View } from 'react-native';
 import Header from './Header'
-export default class Movies extends Component {
+export default class GoogleCivicAPI extends Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -11,15 +11,22 @@ export default class Movies extends Component {
   }
 
   componentDidMount() {
-    return fetch('https://facebook.github.io/react-native/movies.json')
+    return fetch('https://www.googleapis.com/civicinfo/v2/representatives?key=AIzaSyA4cErjEnnOiGVvGHViWcl_sGXsM4CWdjI&address=1263%20Pacific%20Ave.%20Kansas%20City%20KS')
       .then((response) => response.json())
       .then((responseJson) => {
+        // console.log(responseJson)
+        var len = responseJson.offices.length;
+          for (var i = 0; i < len; i++){
+              console.log(i);
+              responseJson.offices[i].official = responseJson.officials[i];
+          }
         let ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
         this.setState({
           isLoading: false,
-          dataSource: ds.cloneWithRows(responseJson.movies),
+          dataSource: ds.cloneWithRows(responseJson.offices),
         }, function() {
-          // do something with new state
+            // console.log(this.state.dataSource.s1[0])
+            // console.log(responseJson)
         });
       })
       .catch((error) => {
@@ -31,19 +38,29 @@ export default class Movies extends Component {
     if (this.state.isLoading) {
       return (
         <View style={{flex: 1, paddingTop: 20}}>
-        <Header/>
+        
           <ActivityIndicator />
 
         </View>
       );
     }
+console.log('DATASOURCE');
+console.log(this.state.dataSource);
+console.log(this.state.dataSource[0]);
+
 
     return (
       <View style={{flex: 1, paddingTop: 20}}>
+      <Header headerText={'GotGov'}/>
         <ListView
           dataSource={this.state.dataSource}
-          renderRow={(rowData) => <Text>{rowData.title}, {rowData.releaseYear}</Text>}
+          renderRow={(rowData) => <Text>
+          {rowData.name}:  {rowData.official.name}
+          
+          
+          </Text>}
         />
+       
       </View>
     );
   }
